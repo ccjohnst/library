97b6d69e80dacfb1cd81c79e6d48c603756e75eb
Chris Johnston <christopherjohnston@protonmail.com>
2021-03-21 21:41:36 +0000

    Added read button

--- js/scripts.js
+++ js/scripts.js
@@ -5,19 +5,21 @@ const container = document.querySelector('.cardsContainer');
 const newBookButton = document.querySelector('#newBookButton')
 newBookButton.addEventListener("click", addBookToLibrary);
 
-const delBookButton = document.querySelectorAll('.deleteButton');
-delBookButton.forEach((button) => {
-    button.addEventListener('click', () => {
-        console.log('success')
-    })
-})
-
 // Create book object
-function Book(title, author, pages) {
+function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
+    this.read = read
+
+    this.markRead = function() {
+       this.read = "Yes";
+    }
 }
+
+// Book.prototype.markAsRead = function () {
+//     this.read + 'Yes';
+// }
 
 /* Takes user's input and stores it into an array. 
 TODO:
@@ -27,6 +29,7 @@ function addBookToLibrary() {
     bookToAdd.title = prompt("Please enter a title");
     bookToAdd.author = prompt("Please enter an author");
     bookToAdd.pages = prompt("Please enter the pages");
+    bookToAdd.read = prompt("Have you read this?", "Yes");
     myLibrary.push(bookToAdd);
     createDisplayBook(bookToAdd);
 }
@@ -35,26 +38,55 @@ function addBookToLibrary() {
 TODO:
 - Set a variable to the current index of item, and use to create set attribute no.*/
 function createDisplayBook(item) {
+    // Get the index of the last array obj
+    const lastIndexNum = myLibrary.length - 1;     
+    
+    // Create divs containing Book obj info
     const cardContent = document.createElement('div');
-    const delButton = document.createElement('button')
-    const lastIndexNum = myLibrary.length - 1; // Get the index of the last array obj
     cardContent.setAttribute("id", `book${lastIndexNum}`); // Make attribute equal to the array
-    delButton.setAttribute("id", `book${lastIndexNum}`);
+    cardContent.classList.add('cardContent');
+    cardContent.innerHTML = `<p id="Title${ lastIndexNum }"> Title: ${ item.title }</p>
+    <p id="Author${ lastIndexNum }">Author: ${ item.author }</p>
+    <p id="Pages${ lastIndexNum }">Pages: ${ item.pages }</p>
+    <p id="Read${ lastIndexNum }">Read: ${ item.read }</p>`;
+    
+    // Create the delete buttons
+    const delButton = document.createElement('button');
     delButton.classList.add("deleteButton");
     delButton.textContent  = "DELETE";
-    cardContent.classList.add('cardContent');
-    cardContent.innerHTML = `<p> Title: ${ item.title }</p><p>Author: ${ item.author }</p><p>Pages: ${ item.pages }</p>`;
+
+    // Create our read status button
+    const readButton = document.createElement('button');
+    readButton.classList.add("readButtons");
+    readButton.setAttribute("id", `button${ lastIndexNum }`);
+    readButton.textContent = "MARK AS READ";
+
+
+    // Add delete button to Object div and add Object div to the container
     cardContent.appendChild(delButton);
+    cardContent.appendChild(readButton);
     container.appendChild(cardContent);
 
-    /*Delete book button */
+    /* Delete book button */
     const delBookButton = document.querySelectorAll('.deleteButton');
     delBookButton.forEach((button) => {
-    button.addEventListener('click', () => {
-        button.parentElement.remove(); 
+        button.addEventListener('click', () => {
+            button.parentElement.remove(); 
+        })
     })
-})
+
+    /* Mark as Read button */
+    const markAsReadButton = document.querySelectorAll('.readButtons');
+    markAsReadButton.forEach((button) => {
+        button.addEventListener('click', () => {
+            item.read = "Yes";
+            let readUpdate = document.getElementById(`Read${ lastIndexNum }`);
+
+            return readUpdate.innerHTML = `Read: ${ item.read } `
+        })
+    })
 }
+
 
 /* Display all booked stored in myLibrary array to page. 
 Run forEach on our createdisplay book function */
@@ -62,7 +94,3 @@ function displayAllBooks() {
     return myLibrary.forEach(createDisplayBook);
 }
 
-// function deleteBook(x) {
-//     let el = getElementById(x)
-//     el.remove();
-// }
